5eb1d0bf04e4edda687e79b1a1b8826a4f8c0be2
PlescaVasile <mereu1visator@gmail.com>
2025-03-07 11:55:18 +0100

    Add vercel.json configuration file

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const client = new Client({
-    user: process.env.DB_USER,        
-    password: process.env.DB_PASSWORD, 
-    database: process.env.DB_NAME, 
+    user: process.env.DB_USER='postgres',       
+    password: process.env.DB_PASSWORD='universae', 
+    database: process.env.DB_NAME || 'empresa', 
     host: process.env.DB_HOST || 'localhost',
     port: process.env.DB_PORT || 5432,           
 });
